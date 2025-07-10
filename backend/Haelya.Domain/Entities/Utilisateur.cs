e920da5611df0d8057f5dd089709120b6d029bf3
Max-Schaekels <ms.noctis@gmail.com>
2025-07-10 17:16:50 +0200

    Initialisation du backend : structure du projet, entité Utilisateur et configuration EF

---
+++ backend/Haelya.Domain/Entities/Utilisateur.cs
@@ -0,0 +1,28 @@
+﻿using Haelya.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haelya.Domain.Entities
+{
+    public class Utilisateur
+    {
+
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string HashPassword { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
---
+++ backend/Haelya.Infrastructure/Configurations/UtilisateurConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using Haelya.Domain.Entities;
+using Haelya.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haelya.Infrastructure.Configurations
+{
+    public class UtilisateurConfiguration : IEntityTypeConfiguration<Utilisateur>
+    {
+        public void Configure(EntityTypeBuilder<Utilisateur> builder)
+        {
+            builder.ToTable("Utilisateur");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder.Property(u => u.HashPassword)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            
+            builder.Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            
+            builder.Property(u => u.Address)
+                .HasMaxLength(255);
+
+            builder.Property(u => u.City)
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Country)
+                .HasMaxLength(100);
+
+            builder.Property(u => u.PostalCode)
+                .HasMaxLength(20);
+
+            
+            builder.Property(u => u.PhoneNumber)
+                .HasMaxLength(20);
+
+            
+            builder.Property(u => u.BirthDate)
+                .HasColumnType("date");
+
+            
+            builder.Property(u => u.RegisterDate)
+                .HasColumnType("datetime");
+
+
+            builder.Property(u => u.Role)
+                .IsRequired()
+                .HasConversion<string>()
+                .HasMaxLength(50)
+                .HasDefaultValue(Role.Customer);
+        }
+    }
+}
