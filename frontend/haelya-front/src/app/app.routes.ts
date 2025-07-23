9954d254cd0ddf739978c1584a7187119f71aac6
Max-Schaekels <ms.noctis@gmail.com>
2025-07-23 13:36:22 +0200

    feat: amélioration formulaire de profil + correction affichage après update

--- frontend/haelya-front/src/app/app.routes.ts
+++ frontend/haelya-front/src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
+import { ProfilComponent } from './pages/user/dashboard/profil/profil.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent},
-    { path:'register', component: RegisterComponent}
+    { path:'register', component: RegisterComponent},
+    {
+        path: 'dashboard', component: DashboardComponent,
+        children: [
+            { path: '', redirectTo: 'profil', pathMatch: 'full'},
+            { path: 'profil', component: ProfilComponent}
+            
+        ]
+    }
 ];
---
+++ frontend/haelya-front/src/app/pages/user/dashboard/profil/profil.component.ts
@@ -0,0 +1,124 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { UserService } from '../../../../core/services/user.service';
+import { ToastService } from '../../../../core/services/toast.service';
+import { User } from '../../../../core/models/user';
+import { UpdateUser } from '../../../../core/models/update-user';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+export const fadeInOut = trigger('fadeInOut', [
+  transition(':enter', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
+  transition(':leave', [animate('200ms', style({ opacity: 0 }))])
+]);
+
+@Component({
+  selector: 'app-profil',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './profil.component.html',
+  styleUrl: './profil.component.scss',
+  animations: [fadeInOut]
+})
+export class ProfilComponent implements OnInit {
+  private _fb: FormBuilder = inject(FormBuilder);
+  private _authService: AuthService = inject(AuthService);
+  private _userService: UserService = inject(UserService);
+  private _toast: ToastService = inject(ToastService);
+  private _routeur: Router = inject(Router);
+
+  isSubmitting: boolean = false;
+  errorMessage: string | null = null;
+  isEditing: boolean = false;
+
+  updateProfilForm!: FormGroup;
+  user!: User;
+
+
+  ngOnInit(): void {
+    this._userService.getprofile().subscribe({
+      next: (data) => {
+        this.user = data;
+        this.errorMessage = null;
+        this.updateProfilForm = this._fb.group({
+          lastName: [this.user.lastName, [Validators.required, Validators.maxLength(55)]],
+          firstName: [this.user.firstName, [Validators.required, Validators.maxLength(55)]],
+          phoneNumber: [this.user.phoneNumber],
+          birthDate: [this.user.birthDate, [this.dateNonFutureValidator()]]
+
+        });
+
+      },
+      error: (err) => {
+        this.errorMessage = err.error?.message || 'Une erreur est survenue pendant la récupération du profil';
+      }
+    });
+  }
+
+  dateNonFutureValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const val = control.value;
+      if (val && new Date(val) > new Date()) {
+        return { futureDate: true };
+      }
+      return null;
+    };
+  }
+
+  onSubmit(): void {
+    if (this.updateProfilForm.invalid) return;
+
+    this.isSubmitting = true;
+    this.errorMessage = null;
+
+    const dto: UpdateUser = {
+      lastName: this.updateProfilForm.value.lastName,
+      firstName: this.updateProfilForm.value.firstName,
+      phoneNumber: this.updateProfilForm.value.phoneNumber,
+      birthDate: this.updateProfilForm.value.birthDate
+    };
+
+    this._userService.updateProfil(dto).subscribe({
+      next: () => {
+        this._userService.getprofile().subscribe({
+          next: (data) => {
+            this.user = data;
+            this.isSubmitting = false;
+            this.isEditing = false;
+            this.updateProfilForm.reset();
+            this.errorMessage = null;
+            this._toast.showToast('Mise à jour du profil réussie !');
+          },
+          error: () => {
+            this.isSubmitting = false;
+            this.isEditing = false;
+            this._toast.showToast('Mise à jour réussie, mais une erreur est survenue lors du rafraîchissement des données.');
+          }
+        });
+
+
+      },
+      error: (err) => {
+        this.isSubmitting = false;
+        this.errorMessage = err.error?.message || 'Une erreur est survenue pendant la mise à jour.';
+      }
+    });
+
+  }
+
+  enableEdit(): void {
+    this.isEditing = true;
+  }
+
+  cancelEdit(): void {
+    this.isEditing = false;
+    this.updateProfilForm.reset({
+      lastName: this.user.lastName,
+      firstName: this.user.firstName,
+      phoneNumber: this.user.phoneNumber,
+      birthDate: this.user.birthDate
+    });
+  }
+
+}
