2540ff9e3ad353f2452239764c99dc6c2bb893ae
Max-Schaekels <ms.noctis@gmail.com>
2025-07-23 14:07:48 +0200

    feat: ajout de guards

--- frontend/haelya-front/src/app/app.routes.ts
+++ frontend/haelya-front/src/app/app.routes.ts
@@ -4,17 +4,19 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
 import { ProfilComponent } from './pages/user/dashboard/profil/profil.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent},
-    { path:'register', component: RegisterComponent},
+    { path: 'register', loadComponent: () => import('./pages/auth/register/register.component').then(m => m.RegisterComponent) },
     {
-        path: 'dashboard', component: DashboardComponent,
-        children: [
-            { path: '', redirectTo: 'profil', pathMatch: 'full'},
-            { path: 'profil', component: ProfilComponent}
-            
-        ]
+    path: 'dashboard',
+    canActivate: [authGuard],
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'profil', pathMatch: 'full' },
+      { path: 'profil', loadComponent: () => import('./pages/user/dashboard/profil/profil.component').then(m => m.ProfilComponent) },     
+    ]
     }
 ];
